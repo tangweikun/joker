31ab96366797aac12acefda14967dd00f5f3d63f
tangweikun <819573105@qq.com>
2018-10-31 22:18:09 +0800

    :wrench: Add fontAwesome

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
-import get from 'lodash/get'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGhost, faAddressCard } from '@fortawesome/free-solid-svg-icons'
 import './App.css'
 import Github from 'pages/github'
+
+library.add(faGhost)
+library.add(faAddressCard)
 
 class App extends Component {
   render() {
