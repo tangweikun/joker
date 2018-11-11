7edddf3aa6632cbb53ee3810dfb659114d96907f
tangweikun <819573105@qq.com>
2018-11-11 16:03:28 +0800

    Updates

---
+++ precache-manifest.8e97a9d759fa4275d56d1ce460fb5b91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "355f0443ec7c679a36ae",
+    "url": "/joker/static/js/runtime~main.355f0443.js"
+  },
+  {
+    "revision": "83c3cdb291a8e99ee4cd",
+    "url": "/joker/static/js/main.83c3cdb2.chunk.js"
+  },
+  {
+    "revision": "51e2633c85acb9c586e8",
+    "url": "/joker/static/js/1.51e2633c.chunk.js"
+  },
+  {
+    "revision": "83c3cdb291a8e99ee4cd",
+    "url": "/joker/static/css/main.6599d976.chunk.css"
+  },
+  {
+    "revision": "51e2633c85acb9c586e8",
+    "url": "/joker/static/css/1.67ce0ea5.chunk.css"
+  },
+  {
+    "revision": "1483dbfa92822914bce89e01115bdf8c",
+    "url": "/joker/index.html"
+  }
+];
